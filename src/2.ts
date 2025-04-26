5c8be31d6a706194b3ab3fe513dc582e882332e6
Oleksandr Pavlishchev <velogitara@gmail.com>
2025-04-26 17:23:41 +0400

    chore(ts-homework-03): add solutions for 3  TypeScript tasks, 4th in progress

--- src/2.ts
+++ src/2.ts
@@ -15,25 +15,22 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+    constructor(
+        public name: string,
+        private department: string,
+        protected salary: number
+    ) {}
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
-
-  getEmployeeDetails() {
-    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
-  }
+    getEmployeeDetails() {
+        return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+    }
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+    // Реалізуйте конструктор та збільшіть salary на 10000
+    constructor(name: string, department: string, salary: number) {
+        super(name, department, salary + 10000); // Виклик конструктора суперкласу
+    }
 }
-
 
 export {};
--- src/3.ts
+++ src/3.ts
@@ -8,25 +8,35 @@
 */
 
 // реалізація класу Wizard
+interface ICharacter {
+    name: string;
+    level: number;
+    introduce(phrase: string): void;
+    levelUp(number: number): void;
+}
+interface ISpellCaster {
+    castSpell(): void;
+}
+
 class Wizard implements ICharacter, ISpellCaster {
-  constructor(public name: string, public level: number) {
-    if (this.level < 1) {
-      throw new Error('Level too low');
+    constructor(public name: string, public level: number) {
+        if (this.level < 1) {
+            throw new Error('Level too low');
+        }
     }
-  }
 
-  introduce(phrase: string): void {
-    console.log(phrase + ', ' + this.name);
-  }
+    introduce(phrase: string): void {
+        console.log(phrase + ', ' + this.name);
+    }
 
-  castSpell(): void {
-    console.log('Casting a spell, behold my power!');
-  }
+    castSpell(): void {
+        console.log('Casting a spell, behold my power!');
+    }
 
-  levelUp(): void {
-    this.level++;
-    console.log(`Level up! New level is ${this.level}`);
-  }
+    levelUp(): void {
+        this.level++;
+        console.log(`Level up! New level is ${this.level}`);
+    }
 }
 
 // тестування класу
@@ -34,6 +44,6 @@ const wizard = new Wizard('Merlin', 15);
 
 wizard.introduce('I am the mighty wizard');
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
+wizard.levelUp(); // Level up! New level is 16
 
 export {};
